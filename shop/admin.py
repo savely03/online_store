bfc9be69268383a477d330ac695a1a625bc5fef7
Savely <savely.domnikov@yandex.ru>
2022-10-18 22:32:51 +0300

    Added product and category models

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Category, Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'price', 'stock', 'available', 'created', 'updated')
+    list_display_links = ('name', 'slug')
+    list_filter = ('available', 'created', 'updated')
+    list_editable = ('price', 'stock', 'available')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    list_display_links = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
 
 # Register your models here.
