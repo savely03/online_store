f3d72b5d122ffe18552877b034df1955cc5b49f6
Savely <savely.domnikov@yandex.ru>
2022-10-20 22:18:30 +0300

    Added Cart

--- online_store/settings.py
+++ online_store/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "shop",
+    "cart",
 ]
 
 MIDDLEWARE = [
@@ -113,3 +114,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
